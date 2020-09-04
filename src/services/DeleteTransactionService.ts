eb4bd4314f9ed19d608e4bd352c4431d99bbc682
Vinicius Santos <santosvini.rv@gmail.com>
2020-09-04 16:12:32 -0300

    123
    123

---
+++ src/services/DeleteTransactionService.ts
@@ -0,0 +1,20 @@
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+
+import Transactionsrepository from '../repositories/TransactionsRepository';
+
+class DeleteTransactionService {
+  public async execute(id: string): Promise<void> {
+    const transactionsRepository = getCustomRepository(Transactionsrepository);
+
+    const transaction = await transactionsRepository.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('Transaction does not exists.');
+    }
+
+    await transactionsRepository.remove(transaction);
+  }
+}
+
+export default DeleteTransactionService;
